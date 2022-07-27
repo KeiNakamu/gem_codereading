a67b897b2f43f4fe3e84c8a5e82f8bcf6b8288fe
Kei <kei.n140802@gmail.com>
2022-07-27 23:58:47 +0900

    envファイル作成

--- sample.rb
+++ sample.rb
@@ -1,7 +1,7 @@
 require "slack-notify"
 require 'clockwork'
 
-sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
+sample = SlackNotify::Client.new(webhook_url: ENV['KEY'])
 sample.notify("ok!!!!!!!!!")
 
 require 'active_support/time'
@@ -9,7 +9,7 @@ require 'active_support/time'
 module Clockwork
   
   every(3.minutes, 'ok') do  
-  sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
+  sample = SlackNotify::Client.new(webhook_url: ENV['KEY'])
   sample.notify("ok!!!!!!!!!")
   end
 end
